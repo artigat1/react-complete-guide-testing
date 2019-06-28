b37fee8a10dc1582e27d261f49be0aff181e9ac0
Steve Doggett <artigat1@users.noreply.github.com>
2019-06-28 15:49:21 +0100

    Added prettier and fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import TodoHeader from './components/Header';
+import TodoList from './components/TodoList';
+import TodoForm from './components/Form';
+
+class App extends Component {
+    state = {
+        todoItems: this.props.initItems
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.addItem = this.addItem.bind(this);
+        this.removeItem = this.removeItem.bind(this);
+        this.markTodoDone = this.markTodoDone.bind(this);
+    }
+
+    addItem(todoItem) {
+        const items = this.state.todoItems;
+        items.unshift({
+            index: this.state.todoItems.length + 1,
+            value: todoItem.newItemValue,
+            done: false
+        });
+        this.setState({ todoItems: items });
+    }
+
+    removeItem(itemIndex) {
+        const items = this.state.todoItems;
+        items.splice(itemIndex, 1);
+        this.setState({ todoItems: items });
+    }
+
+    markTodoDone(itemIndex) {
+        const items = this.state.todoItems;
+        var todo = items[itemIndex];
+        items.splice(itemIndex, 1);
+        todo.done = !todo.done;
+        todo.done ? items.push(todo) : items.unshift(todo);
+        this.setState({ todoItems: items });
+    }
+
+    render() {
+        console.log('state', this.state);
+        return (
+            <div id="main">
+                <TodoHeader />
+                <TodoList
+                    items={this.state.todoItems}
+                    removeItem={this.removeItem}
+                    markTodoDone={this.markTodoDone}
+                />
+                <TodoForm addItem={this.addItem} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var todoItems = [];
+todoItems.push({ index: 1, value: 'learn react', done: false });
+todoItems.push({ index: 2, value: 'Go shopping', done: true });
+todoItems.push({ index: 3, value: 'buy flowers', done: true });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App initItems={todoItems} />, document.getElementById('root'));
